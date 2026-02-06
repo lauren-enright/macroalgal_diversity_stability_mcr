1c57cd9934e3f46ae3d3aebc0284b56a92f066bd
lauren-enright <115184517+lauren-enright@users.noreply.github.com>
2026-02-06 14:48:51 -0800

    diversity cover models and figures

---
+++ 03a_diversity_cover_models.R
@@ -0,0 +1,123 @@
+rm(list = ls())
+library(here)
+library(tidyverse)
+library(glmmTMB)
+library(emmeans)
+library(car)
+library(effects)
+library(DHARMa)
+library(multcomp)
+
+#source("01b_data_prep.R")
+source("00_functions_and_aes.R")
+
+alpha_diversity_quad_macro <- read.csv(here::here("data", "alpha_diversity_quad_macro_09262025.csv"))
+#alpha_diversity_site_macro <- read.csv(here::here("data", "alpha_diversity_site_macro.csv"))
+
+alpha_diversity_quad_macro$habitat <- factor(alpha_diversity_quad_macro$habitat,
+                     levels = c("Fringing", "Backreef", "Forereef 10m", "Forereef 17m"))
+
+cor(alpha_diversity_quad_macro$richness, alpha_diversity_quad_macro$functional_richness, use = "complete.obs", method = "pearson")
+cor.test(alpha_diversity_quad_macro$richness, alpha_diversity_quad_macro$functional_richness, method = "pearson")   # or "spearman"
+
+
+#### COVER ~ RICHNESS ####
+cover_mod <- glmmTMB(cover_trans ~ richness*habitat + (1|site/location) + (1|year), family = beta_family(), data = alpha_diversity_quad_macro)
+summary(cover_mod)
+car::Anova(cover_mod)
+
+#Response: cover_trans
+#Chisq Df Pr(>Chisq)    #this matches! 
+#richness         30916.15  1  < 2.2e-16 ***
+#  habitat            141.77  3  < 2.2e-16 ***
+#  richness:habitat   294.57  3  < 2.2e-16 ***
+
+hist(residuals(cover_mod)) # looks fine
+plot(residuals(cover_mod) ~ fitted(cover_mod)) # negative trend but it can't be less than 0 so not too concerned. some wave pattern
+performance::r2(cover_mod) # marginal: 0.635, conditional: 0.654
+em_cover_mod <- emtrends(cover_mod, pairwise ~ habitat, var = "richness") # backreef not different from forereef, forereef also not different
+
+#contrast                    estimate     SE  df z.ratio p.value
+#Fringing - Backreef          0.34720 0.0241 Inf  14.409  <.0001
+#Fringing - Forereef 10m      0.34865 0.0240 Inf  14.514  <.0001
+#Fringing - Forereef 17m      0.38953 0.0250 Inf  15.599  <.0001
+#Backreef - Forereef 10m      0.00144 0.0205 Inf   0.071  0.9999
+#Backreef - Forereef 17m      0.04232 0.0214 Inf   1.979  0.1957
+#Forereef 10m - Forereef 17m  0.04088 0.0209 Inf   1.952  0.2063
+
+cld_cover_mod <- multcomp::cld(em_cover_mod, Letters = letters, sort = FALSE)
+
+#### COVER ~ FUNCTIONAL RICHNESS ####
+#check number of functional richness categories
+unique(alpha_diversity_quad_macro$functional_richness)
+
+cover_mod_fg <- glmmTMB(cover_trans ~ functional_richness*habitat + (1|site/location) + (1|year), family = beta_family(), data = alpha_diversity_quad_macro)
+
+
+summary(cover_mod_fg)
+car::Anova(cover_mod_fg)
+
+#Lauren - old, mistake, with 9 functional groups
+#Chisq Df Pr(>Chisq)    
+#functional_richness         31722.867  1  < 2.2e-16 ***
+#habitat                        48.585  3  1.598e-10 ***
+#functional_richness:habitat   455.808  3  < 2.2e-16 ***
+
+#NEW - with 8 functional groups
+#functional_richness         31789.564  1  < 2.2e-16 ***
+# habitat                        48.108  3   2.02e-10 ***
+# functional_richness:habitat   457.578  3  < 2.2e-16 ***
+
+hist(residuals(cover_mod_fg)) # good
+performance::r2(cover_mod_fg) # Noam: margingal = 0.63, conditional = 0.65 #Lauren = same --> these are the same for 8 fg:)
+em_cover_mod_fg <- emtrends(cover_mod_fg, pairwise ~ habitat, var = "functional_richness") 
+
+#contrast                    estimate     SE  df z.ratio p.value
+#Fringing - Backreef            0.225 0.0265 Inf   8.500  <.0001
+#Fringing - Forereef 10m        0.357 0.0261 Inf  13.701  <.0001
+#Fringing - Forereef 17m        0.536 0.0262 Inf  20.499  <.0001
+#Backreef - Forereef 10m        0.132 0.0231 Inf   5.697  <.0001
+#Backreef - Forereef 17m        0.311 0.0231 Inf  13.435  <.0001
+#Forereef 10m - Forereef 17m    0.179 0.0223 Inf   8.061  <.0001
+
+#new - 8 functional groups, only small changes! 
+#$contrasts
+#contrast                    estimate     SE  df z.ratio p.value
+#Fringing - Backreef            0.223 0.0265 Inf   8.404  <.0001
+#Fringing - Forereef 10m        0.357 0.0260 Inf  13.699  <.0001
+#Fringing - Forereef 17m        0.536 0.0262 Inf  20.502  <.0001
+#Backreef - Forereef 10m        0.134 0.0231 Inf   5.813  <.0001
+#Backreef - Forereef 17m        0.314 0.0231 Inf  13.562  <.0001
+#Forereef 10m - Forereef 17m    0.180 0.0223 Inf   8.067  <.0001
+
+cld_cover_mod_fg <- multcomp::cld(em_cover_mod_fg, Letters = letters, sort = FALSE)
+
+#pull out data from models to make supplemental figures:
+
+#richness by cover
+t.s2.a <- as.tibble(cld_cover_mod) %>%
+  mutate(Predictor = "Taxonomic richness",
+         `Spatial scale` = "Plot-level") %>% 
+  # rename this so it matches for joining purposes
+  rename_if(str_detect(names(.), ".trend"), ~"Mean")
+#functional richness by cover
+
+t.s2.b <-  as.tibble(cld_cover_mod_fg) %>%
+  mutate(Predictor = "Functional richness",
+         `Spatial scale` = "Plot-level") %>% 
+  rename_if(str_detect(names(.), ".trend"), ~"Mean")
+
+#joining together
+
+rbind(t.s2.a, t.s2.b) %>% 
+  mutate(Table = "S2_plotlevel") %>% 
+  dplyr::select(-SE, -df) -> table_s2_plot
+
+supplemental_tables_tableS2_plot <- table_s2_plot %>% 
+  # rename to match previous version/code
+  dplyr::rename(Habitat = habitat,
+         Lower_CI = asymp.LCL,
+         Upper_CI = asymp.UCL)
+
+
+
---
+++ 03b_diversity_cover_figures.R
@@ -0,0 +1,142 @@
+rm(list = ls())
+source("03a_diversity_cover_models.R")
+library(here)
+library(tidyverse)
+library(glmmTMB)
+library(emmeans)
+library(car)
+library(effects)
+library(DHARMa)
+library(ggplot2)
+library(patchwork)
+library(ggpubr)
+
+
+#### COVER ~ RICHNESS ####
+# SETUP
+
+diversity_ranges_quad <- extract_ranges(alpha_diversity_quad_macro, "habitat", c("richness", "cover_trans", "functional_richness"))
+
+cover_emm <- emmip(cover_mod,
+                   habitat ~ richness,
+                   at = list(richness = seq(0,10,1)), plotit = F, CIs = T)
+
+
+
+filtered_cover_effects <- filter_ranges(trend = cover_emm, range_obj = diversity_ranges_quad, group = "habitat", value = "richness")
+
+# PLOT
+(figure_2a <- 
+    ggplot() +
+#    geom_point(data = alpha_diversity_quad_macro,
+#               aes(x = richness, y = cover_trans, colour = habitat), size = 1, alpha = 0.75) + 
+    geom_line(data = filtered_cover_effects, aes(x = richness, y = inv_logit(yvar), colour = habitat)) +
+    geom_ribbon(data = filtered_cover_effects, aes(x = richness, ymin = inv_logit(LCL), ymax = inv_logit(UCL), fill = habitat),
+                alpha = 0.5) +
+    scale_colour_manual(values = habitat_colours, labels = habitat_labels) +
+    scale_fill_manual(values = habitat_colours, labels = habitat_labels) +
+    labs(y = "Cover\n", x = "Taxonomic richness", title = "") +
+    model_themes 
+)
+
+#### COVER ~ FUNCTIONAL RICHNESS ####
+# SETUP
+cover_emm_fg <- emmip(cover_mod_fg,
+                      habitat ~ functional_richness,
+                      at = list(functional_richness = seq(0,10,1)), plotit = F, CIs = T)
+
+filtered_cover_effects_fg <- filter_ranges(cover_emm_fg, diversity_ranges_quad, "habitat", "functional_richness")
+
+# PLOT
+(figure_2b <- 
+    ggplot() +
+    #geom_jitter(data = alpha_diversity_quad_macro,
+             #  aes(x = functional_richness, y = cover_trans, colour = habitat), width = 0.2, size = 1, alpha = 0.1) + 
+    geom_line(data = filtered_cover_effects_fg, aes(x = functional_richness, y = inv_logit(yvar), colour = habitat)) +
+    geom_ribbon(data = filtered_cover_effects_fg, aes(x = functional_richness, ymin = inv_logit(LCL), ymax = inv_logit(UCL), fill = habitat),
+                alpha = 0.5) +
+    scale_colour_manual(values = habitat_colours, labels = habitat_labels) +
+    scale_fill_manual(values = habitat_colours, labels = habitat_labels) +
+    labs(y = "", x = "Functional richness", title = "") +
+    model_themes
+)
+#ggsave(filename = "output/figure_3b_quad_v2.png", figure_3b, height = 10, width = 14)
+
+#### PANEL GRAPH ####
+
+(figure_2 <- ggarrange(figure_2a, figure_2b,  
+                       ncol = 2, nrow = 1, 
+                       common.legend = TRUE,
+                       labels = c("a.", "b."),
+                       legend = "bottom", 
+                       font.label = list(size = 26, color = "black", face = "plain")) )
+
+#ggsave(filename = "output/figure2_11212024.jpg", figure_2, height = 8, width = 16)
+
+#adding supplemental figures
+
+#want taxonomic richness as first panel to match
+
+supplemental_tables_tableS2_plot <- supplemental_tables_tableS2_plot %>%
+  mutate(Predictor = factor(Predictor, 
+                          levels = c("Taxonomic richness", "Functional richness")))
+
+supplemental_tables_tableS2_plot %>% 
+  filter(Table == "S2_plotlevel") %>% 
+  filter(Predictor == "Taxonomic richness") %>% 
+  # order the habitats by distance from shore
+  mutate(Habitat = factor(Habitat, levels = c("Forereef 17m", "Forereef 10m",  "Backreef", "Fringing"))) %>% 
+  ggplot(aes(x = Mean, y = Habitat, color = Habitat)) +
+  geom_point(size = 5) +
+  # add confidence intervals:
+  geom_linerange(aes(xmin = Lower_CI, xmax = Upper_CI), linewidth = 3) +
+  scale_colour_manual(values = habitat_colours, label = habitat_labels) +
+  xlab("Coefficient") +
+  geom_vline(xintercept = 0, linetype = "dashed", linewidth = 1, color = "gray") +
+  ylab("") +
+  ggtitle("a. Taxonomic Richness") +
+  model_themes + 
+  # add letters denoting significance
+  geom_text(aes(x = Upper_CI + 0.10 , y = Habitat, 
+                label = .group), colour = "black", size = 10) +
+  scale_y_discrete(labels = c(
+    "Fringing"     = "Fringing reef",
+    "Backreef"     = "Back reef",
+    "Forereef 10m" = "Fore reef 10 m",
+    "Forereef 17m" = "Forereef 17 m"
+  )) +
+  guides(color = guide_legend(reverse = TRUE)) -> s2.taxo
+
+supplemental_tables_tableS2_plot %>% 
+  filter(Table == "S2_plotlevel") %>% 
+  filter(Predictor == "Functional richness") %>% 
+  # order the habitats by distance from shore
+  mutate(Habitat = factor(Habitat, levels = c("Forereef 17m", "Forereef 10m",  "Backreef", "Fringing"))) %>% 
+  ggplot(aes(x = Mean, y = Habitat, color = Habitat)) +
+  geom_point(size = 5) +
+  # add confidence intervals:
+  geom_linerange(aes(xmin = Lower_CI, xmax = Upper_CI), linewidth = 3) +
+  scale_color_manual(values = habitat_colours, label = habitat_labels) +
+  xlab("Coefficient") +
+  geom_vline(xintercept = 0, linetype = "dashed", linewidth = 1, color = "gray") +
+  ylab("") +
+  ggtitle("b. Functional Richness") +
+  scale_y_discrete(labels = c(
+    "Fringing" = "Fringing reef",
+    "Backreef" = "Back reef",
+    "Forereef 10m" = "Fore reef 10 m",
+    "Forereef 17m" = "Fore reef 17 m"
+  )) +
+  model_themes + 
+ # theme(legend.position = "none") +
+  # add letters denoting significance
+  geom_text(aes(x = Upper_CI + 0.10 , y = Habitat, 
+                label = .group), colour = "black", size = 10) +
+  guides(color = guide_legend(reverse = TRUE)) -> s2.functional
+
+s2_figure <- s2.taxo + s2.functional +  plot_layout(guides = "collect") &          # collect legends into one
+  theme(legend.position = "bottom")  
+
+#ggsave(filename = "output/Supp_FigS2_121625.jpg", s2_figure, height = 10, width = 22)
+
+
